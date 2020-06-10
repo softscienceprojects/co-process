97de2cdf9b3caa189164db847e9c942222a29a06
erin nicole johnson <erin.nicole.johnson@gmail.com>
2020-06-10 14:05:34 +0100

    starting to block out User

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,21 @@
 from flask import Flask
-import routes
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thiswillbechangedsoon'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coprocess.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+    def __repr__(self):
+        return f"User ('{self.username}', '{self.email}', '{self.image_file}')"
+    
+
 
